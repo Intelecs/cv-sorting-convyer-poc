b6fe5c6af1d414d416e102d55c83997f148206e1
Benedict-Bsl <migarajun@outlook.com>
2022-06-02 07:35:03 +0300

    stepper motor first test controll

---
+++ stepperMotor.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+from RpiMotorLib import RpiMotorLib
+import time
+
+dir = 22
+step = 23
+en_pin = 24
+
+
+motor = RpiMotorLib.A4988Nema(dir, step, (21,21,21), "DRV8825")
+GPIO.setup(en_pin, GPIO.OUT)
+
+#Motor control
+GPIO.output(en_pin, GPIO.LOW) #pulled to low to enable motor
+motor.motor_go(False, 
+                "Full",
+                200,
+                0.0005,
+                False,
+                0.05)
+
+GPIO.cleanup();
