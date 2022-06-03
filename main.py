8d2b1b2f488389948b7ae6fa9c5dbff6d7627c77
JanuaryThomas <january_thomas@hotmail.com>
2022-06-03 07:15:34 +0300

    adding inference

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from yolov5.device.stepperMotor import run_conveyer
 from threading import Thread
-from time import sleep
+import subprocess
+
+def run_inference():
+    
+    command = "python3 yolov5/detect.py --weights models/best.pt --source 0"
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding='utf-8', universal_newlines=True)
+    while True:
+        realtime_output = process.stdout.readline()
+        if realtime_output != '':
+            print(realtime_output)
 
 
 if __name__ == '__main__':
     
     
     Thread(target=run_conveyer, daemon=False).start()
+    Thread(target=run_inference, daemon=False).start()
     
 
 
