9938db74643cee94af2fe34c1fa302b9db67e501
JanuaryThomas <january_thomas@hotmail.com>
2022-07-03 19:45:13 +0300

    addingconver

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def run_inference():
 
 
 if __name__ == '__main__':
-    Thread(target=run_conveyer, daemon=True).start()
+    # Thread(target=run_conveyer, daemon=True).start()
     Thread(target=run_inference, daemon=True).start()
     
 
