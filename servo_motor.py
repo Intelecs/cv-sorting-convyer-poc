86ede4fe08be3c793130a7b12716cfbfb6fd44ac
JanuaryThomas <january_thomas@hotmail.com>
2022-07-03 19:08:32 +0300

    adding actuators

---
+++ servo_motor.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+
+servoPIN = 13
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
+
+p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+p.start(7.5) # Initialization
+time.sleep(5)
+
+val = 0
+try:
+
+  #block 
+  p.ChangeDutyCycle(3.5)
+  time.sleep(15)
+  #come back to internet
+  p.ChangeDutyCycle(7.5)
+  time.sleep(5)
+
+  
+except KeyboardInterrupt:
+  p.stop()
+  GPIO.cleanup()
+
