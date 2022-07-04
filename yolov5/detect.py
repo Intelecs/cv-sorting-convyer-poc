a98285e54e597875f36b97142c7e76f9dda7ab22
JanuaryThomas <january_thomas@hotmail.com>
2022-07-04 07:31:39 +0300

    adding servoc

--- yolov5/detect.py
+++ yolov5/detect.py
@@ -64,7 +64,7 @@ def open_servo():
 def run_conveyer():
     while True:
         pi.write(DIR, CCW)
-        time.sleep(1)
+        time.sleep(5)
 
 Thread(target=run_conveyer, daemon=True).start()
 
