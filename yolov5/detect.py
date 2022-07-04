24f7f46d5d225bb58aeddd4c6d818f5a2b345042
JanuaryThomas <january_thomas@hotmail.com>
2022-07-04 07:40:52 +0300

    adding servoc

--- yolov5/detect.py
+++ yolov5/detect.py
@@ -45,7 +45,7 @@ SPR = 48   # Steps per Revolution (360 / 7.5)
 
 try:
     pi.set_PWM_dutycycle(STEP, 128)
-    pi.set_PWM_frequency(STEP, 1000)
+    pi.set_PWM_frequency(STEP, 200)
 except Exception as e:
     pass
 
