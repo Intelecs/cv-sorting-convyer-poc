a2ab588d1c2f8c5b4b170b6f3c7b0cb99b478b2c
JanuaryThomas <january_thomas@hotmail.com>
2022-07-05 08:28:40 +0300

    adding duty cylue

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import subprocess
 
 
 def run_inference():
-    command = "python yolov5/detect.py --weights models/best.pt --source 0 --conf-thres 0.05 --imgsz 512"
+    command = "python yolov5/detect.py --weights models/best.pt --source 0 --conf-thres 0.05 --imgsz 640"
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding='utf-8', universal_newlines=True)
     while True:
         realtime_output = process.stdout.readline()
